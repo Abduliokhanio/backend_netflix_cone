40bd8fc4caa3c0a3dee52b2739bb6fbed4af6679
Abduliokhanio <akhan17@outlook.com>
2022-06-17 14:25:24 -0500

    fixed show in the spec

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -1,5 +1,8 @@
 require "rails_helper"
 RSpec.describe MoviesController do
+
+  let(:a_movie_test) { Movie.create(title: "Spec Movie", description: "spec descritpopn") }
+
   context "GET index" do 
     it "renders index" do
       get :index
@@ -11,6 +14,24 @@ RSpec.describe MoviesController do
       get :index
       all_movies_test = Movie.create(title: "Spec Movie", description: "spec descritpopn")
       expect(assigns(:all_movies)).to eq([all_movies_test])
+    end
+  end
+
+  context "GET show" do 
+
+    before(:example) do 
+      a_movie_test
+    end
+
+    it "renders show" do 
+      get :show, params: { id: a_movie_test.id }
+      expect(response.request.filtered_parameters).to eq({"id"=>"1", "controller"=>"movies", "action"=>"show"})
+      expect(response.status).to eq(200)
+    end
+
+    it "assigns @a_movie" do 
+      get :show, params: { id: a_movie_test.id }
+      expect(assigns(:a_movie)).to eq(a_movie_test)
     end
   end
 end
