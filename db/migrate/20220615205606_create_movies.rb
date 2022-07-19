5f1137494c4fad037374fcda404e43f7e9b6d307
Abduliokhanio <akhan17@outlook.com>
2022-07-19 14:55:49 -0500

    successfully migrated to PG

--- db/migrate/20220615205606_create_movies.rb
+++ db/migrate/20220615205606_create_movies.rb
@@ -1,7 +1,7 @@
 class CreateMovies < ActiveRecord::Migration[6.0]
   def change
     create_table :movies do |t|
-      t.string :title
+      t.text :title
       t.text :description
 
       t.timestamps
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Movie.create(title: "Star Wars", description: "A movie about a war in space.")
